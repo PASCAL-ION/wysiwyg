de3eef39273a296206f1d71f83b2fdd18c8f6009
Tom Cardonnel <tomu.cardonnel@gmail.com>
2025-02-02 23:28:27 +0100

    [UPDATE] index.js : option

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { MyWysiwyg } from "./modules/my_wysiwyg.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let mw = new MyWysiwyg(document, {
-      options: ["bold", "italic", "deleted", "color", "font-size", "link"],
-    });
+  let mw = new MyWysiwyg(document, {
+    options: ["bold", "italic", "deleted", "color", "font-size", "link", "align-start", "align-center", "align-end", "align-justify", "html"],
+  });
 });
 
 // document.addEventListener("DOMContentLoaded", () => {
