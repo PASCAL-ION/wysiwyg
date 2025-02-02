5d1de816b81c01bbbdf39b091cb52e14f1fae916
Tom Cardonnel <tomu.cardonnel@gmail.com>
2025-02-02 23:31:31 +0100

    [FIX] oublie de css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { MyWysiwyg } from "./modules/my_wysiwyg.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   let mw = new MyWysiwyg(document, {
-    options: ["bold", "italic", "deleted", "color", "font-size", "link", "align-start", "align-center", "align-end", "align-justify", "html"],
+    options: ["bold", "italic", "color", "font-size", "align-start", "align-center", "align-end", "align-justify", "html"],
   });
 });
 
